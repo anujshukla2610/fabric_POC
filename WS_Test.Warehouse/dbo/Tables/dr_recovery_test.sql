41eee23babb79a21498add1dc26ab52c0de0d49d
anujshukla2610 <anujshukla170223@gmail.com>
2026-09-24 18:31:04 +0530

    Committing 1 item from workspace 092db2e9-3190-4705-83a3-7c56a9884c12

---
+++ WS_Test.Warehouse/dbo/Tables/dr_recovery_test.sql
@@ -0,0 +1,12 @@
+CREATE TABLE [dbo].[dr_recovery_test] (
+    [customer_id]   INT             NULL,
+    [customer_name] VARCHAR (100)   NULL,
+    [city]          VARCHAR (100)   NULL,
+    [state]         VARCHAR (100)   NULL,
+    [customer_type] VARCHAR (50)    NULL,
+    [amount]        DECIMAL (18, 2) NULL,
+    [load_date]     DATE            NULL
+);
+
+
+GO
